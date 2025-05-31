e5156685e8515e41cb1f04656ad70fc8305a2b08
Nazifa Tabassum <132453499+tabassumnazifa@users.noreply.github.com>
2025-05-31 23:49:36 +0600

    temp.cpp

--- Conditional_Statement/temp.cpp
+++ Conditional_Statement/temp.cpp
@@ -1,1 +1,5 @@
-
+Types of Conditional Statements:
+if – Executes code if a condition is true.
+else – Executes code if the if condition is false.
+else if – Tests a new condition if the previous one is false.
+switch – Checks multiple possible values for a variable.
