90ed7f22550d8deee3256152a70bf6521229f967
Nazifa Tabassum <132453499+tabassumnazifa@users.noreply.github.com>
2025-06-02 23:39:59 +0600

    Add files via upload

---
+++ Method_Basic/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+class newclass
+{
+    public static void greet(string name)
+    {
+        Console.WriteLine("Welcome " + name + '\n');
+    }
+}
+class program
+{
+    static void Main()
+    {
+        newclass.greet("Nazifa");
+    }
+}
+    
