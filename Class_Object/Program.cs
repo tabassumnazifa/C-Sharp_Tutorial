d12651de6268bf32c3f4cd87f879ce44cf7e1680
Nazifa Tabassum <132453499+tabassumnazifa@users.noreply.github.com>
2025-06-03 23:38:23 +0600

    Add files via upload

---
+++ Class_Object/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+class mobile
+{
+    int price;
+    string name;
+    void call()
+    {
+        Console.WriteLine("Calling");
+    }
+    static void Main()
+    {
+        mobile m = new mobile();
+        m.price = 10000;
+        m.name = "Samsung";
+        m.call();
+        Console.WriteLine("Mobile name=" + m.name);
+        Console.WriteLine("Mobile price" + m.price);
+    }
+}
